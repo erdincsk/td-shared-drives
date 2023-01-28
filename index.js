3ff0edb9f385b68166dd2a5213256e982108d298
Erdinç <9353938+erdincsk@users.noreply.github.com>
2023-01-28 22:06:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ class mainClass {
     // If available, enter your proxy connection information, it can block creating a proxy site.
     const proxy = "";
 
-    const gmail = "ekeloglu@gmail.com";
-    const school_id = "30";
+    const gmail = "medinerdal29@gmail.com";
+    school_id = "30";
 
     await driveEngine.CreateTDrive(userID, username, gmail, school_id, proxy)
     console.log('Task Fnish ! New task is starting...')
